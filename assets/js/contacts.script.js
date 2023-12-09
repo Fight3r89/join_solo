618ff011ab35d00b76c29f58966ddb594b44eafb
Simon Funke <sim_89@web.de>
2023-12-09 15:50:09 +0100

    slideIn/slideOut animation for add contact finished

--- assets/js/contacts.script.js
+++ assets/js/contacts.script.js
@@ -7,3 +7,20 @@ function navChangeColor(){
     document.getElementById('nav-contacts').classList.add('link-active');
     document.getElementById('nav-contacts').onclick = null;
 }
+
+function slideIn(){
+    document.getElementById('contacts-add-card-container').classList.remove('d-none');
+    document.getElementById('contacts-add-card').style.right = '16px';
+    document.getElementById('contacts-add-card').style.animation = 'slide_in 0.3s ease-out';
+}
+
+function slideOut(){
+    document.getElementById('contacts-add-card').style.animation = 'slide_out 0.3s ease-out';
+    setTimeout (function(){
+        document.getElementById('contacts-add-card-container').classList.add('d-none');
+        document.getElementById('contacts-add-card').style.right = '-150%';
+    },280);
+    
+    
+    
+}
