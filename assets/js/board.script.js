3f96faa1b709d28897ecd7ad9f07c68d1da7a1e2
Simon Funke <sim_89@web.de>
2023-12-11 00:14:46 +0100

    added slider in board for adding Tasks

--- assets/js/board.script.js
+++ assets/js/board.script.js
@@ -8,3 +8,16 @@ function navChangeColor(){
     document.getElementById('nav-board').onclick = null;
 }
 
+function slideIn(){
+    document.getElementById('addtask-card-container').classList.remove('d-none');
+    document.getElementById('addtask-card').style.right = '16px';
+    document.getElementById('addtask-card').style.animation = 'slide_in 0.3s ease-out';
+}
+
+function slideOut(){
+    document.getElementById('addtask-card').style.animation = 'slide_out 0.3s ease-out';
+    setTimeout (function(){
+        document.getElementById('addtask-card-container').classList.add('d-none');
+        document.getElementById('addtask-card').style.right = '-150%';
+    },280);
+}
