041c0c4bc37540471358b138cf68b5cb01242150
Simon Funke <sim_89@web.de>
2024-01-26 19:42:25 +0100

    Single contacts are now shown with the correct data

--- assets/js/contacts.script.js
+++ assets/js/contacts.script.js
@@ -5,7 +5,7 @@ async function init() {
     navChangeColor();
     await getUserDataFromLocalStorage();
     await loadUsersContacts();
-
+    renderContactHtml();
 }
 
 function navChangeColor(){
@@ -35,7 +35,7 @@ function slideOut(container){
     },280); 
 }
 
-function addContact() {
+async function addContact() {
     let nameInput = document.getElementById('contact_name').value;
     let contact = new Contact;
     let firstName;
@@ -55,12 +55,65 @@ function addContact() {
     contact.eMail = document.getElementById('contact_email').value;
     contact.phone = document.getElementById('contact_phone').value;
     contact.assignedTo = loggedInUser.id;
-    saveContactsToBackend(contact);
-
+    await saveContactsToBackend(contact);
+    clearInputFields();
+    slideOut('contacts-add-card');
+    await loadUsersContacts();
+    renderContactHtml();    
 }
 
 function clearInputFields() {
     document.getElementById('contact_name').value = '';
     document.getElementById('contact_email').value = '';
     document.getElementById('contact_phone').value = '';
+}
+
+function renderContactHtml(){
+    clearContactList();
+    renderContactsHtmlCard();
+}
+
+function clearContactList() {
+    document.getElementById('div-contacts-list').innerHTML = '';
+}
+
+function renderContactsHtmlCard(){
+    for (let i = 0; i < userContacts.length; i++) {
+        
+        let inizials = userContacts[i].firstName[0] + userContacts[i].lastName[0];
+        document.getElementById('div-contacts-list').innerHTML += `
+        <div class="div-contacts-list-card" onclick="openSingleContact('single-contact', ${i})">
+            <div class="div-contacts-list-logo">${inizials}</div>
+            <div class="div-contacts-list-data">
+                <p>${userContacts[i].firstName} ${userContacts[i].lastName}</p>
+                <p>${userContacts[i].eMail}</p>
+            </div>
+        </div>`;
+    };
+    
+}
+
+async function renderSingleContact(arrayPosotion){
+    clearSingleContactContainer();
+    renderSingleContactContainerHtml(arrayPosotion);
+}
+
+function clearSingleContactContainer(){
+    document.getElementById('single-contact-logo').innerHTML = '';
+    document.getElementById('single-contact-name').innerHTML = '';
+    document.getElementById('single-contact-data-mail').innerHTML = '';
+    document.getElementById('single-contact-data-phone').innerHTML = '';
+}
+
+function renderSingleContactContainerHtml(arrayPosotion){
+    let inizials = userContacts[arrayPosotion].firstName[0] + userContacts[arrayPosotion].lastName[0]; 
+    document.getElementById('single-contact-logo').innerHTML = inizials;
+    document.getElementById('single-contact-name').innerHTML = userContacts[arrayPosotion].firstName +' '+ userContacts[arrayPosotion].lastName;
+    document.getElementById('single-contact-data-mail').innerHTML = userContacts[arrayPosotion].eMail;
+    document.getElementById('single-contact-data-phone').innerHTML = userContacts[arrayPosotion].phone;
+}
+
+async function openSingleContact(target, arrayPosotion){
+    await renderSingleContact(arrayPosotion);
+    slideIn(target);
 }
