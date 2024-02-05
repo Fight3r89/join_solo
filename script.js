5b4764319a21114d7d79de8b1802e69c4423a894
Simon Funke <sim_89@web.de>
2024-02-05 12:26:34 +0100

    variables are now in seperate js

--- script.js
+++ script.js
@@ -1,21 +1,4 @@
-let tasks = [];
-let users = [];
-let allContacts = [];
-let addSubtask = [];
-let addAssignedTo = [];
-let loggedInUser = [];
-let userTasks = [];
-let userContacts = [];
-let amountOfTasks = 0;
-let tasksTodo;
-let tasksDone;
-let tasksInProgress;
-let tasksAwaitFeedback;
-let tasksUrgent;
-let selected = 'medium';
-let task = 'todo';
-let toggleShowAssignedTo = false;
-let colors = ['#FF7A00', '#00BEE8', '#1FD7C1', '#462F8A', '#6E52FF', '#9327FF', '#9747FF', '#FC71FF', '#FF4646', '#FFBB2B'];
+
 
 
 async function includeHTML() {
